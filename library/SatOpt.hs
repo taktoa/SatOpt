06a1a31b5f66eafd5174cb602dd95a57255af4a7
Remy Goldschmidt <taktoa@gmail.com>
2015-05-02 15:08:45 -0500

    About to switch to HMatrix

--- library/SatOpt.hs
+++ library/SatOpt.hs
@@ -3,14 +3,16 @@
 -- | TODO
 module SatOpt (module SatOpt) where
 
-import           SatOpt.FFT               as SatOpt (fft2d, ifft2d)
-import           SatOpt.Render            as SatOpt
-import           SatOpt.Utility           as SatOpt
+import           SatOpt.FFT                    as SatOpt (fft2d, ifft2d)
+import           SatOpt.Render                 as SatOpt
+import           SatOpt.Utility                as SatOpt
 -- GENERATE: import New.Module as SatOpt
 
 import           Data.Colour.RGBSpace
 import           Data.Colour.RGBSpace.HSL
 import           Data.Complex
+import           Numeric.LinearAlgebra.HMatrix hiding (matrix, size)
+import qualified Numeric.LinearAlgebra.HMatrix as HM
 
 size :: Num a => a
 size = 256
@@ -21,16 +23,15 @@ center x = x - (size / 2)
 cnorm :: Floating a => a -> a -> a
 cnorm x y = norm (center x) (center y)
 
-window :: Double -> Double -> Double
+window :: ℝ -> ℝ -> ℝ
 window x y = (+ 0.5) $ clamp $ subtract 0.5 $ (* (size/8)) $ recip $ cnorm x y
 
-pointSources :: [(Double, Double, Complex Double)] ->
-                Double -> Double -> Complex Double
+pointSources :: [(ℝ, ℝ, ℂ)] -> ℝ -> ℝ -> ℂ
 pointSources [] _ _ = 0
 pointSources ps x y = sum $ map (\(cx, cy, s) -> s * delta cx cy x y) ps
 
 
-testFunc :: Double -> Double -> Complex Double
+testFunc :: ℝ -> ℝ -> ℂ
 testFunc = pointSources [ (0.48, 0.48, 0.5 :+ 0)
                         , (0.52, 0.52, (-0.5) :+ 0)
                         ]
@@ -44,10 +45,10 @@ testFunc = pointSources [ (0.48, 0.48, 0.5 :+ 0)
 --testFunc x y = window x y * sin ((x + y) * 25 / size) :+ 0
 --testFunc x y = sin ((sqrt ((x - (size / 2))^2 + (y - (size / 2))^2)) * 10 * sqrt 2 / size) :+ 0
 
-wavenum :: [[Complex Double]]
+wavenum :: [[ℂ]]
 wavenum = constm 1
 
-im, jm :: [[Complex Double]]
+im, jm :: [[ℂ]]
 im = sample2D (\x _ -> x :+ 0) [0, 1 .. size] [0, 1 .. size]
 jm = sample2D (\_ y -> y :+ 0) [0, 1 .. size] [0, 1 .. size]
 
@@ -64,29 +65,29 @@ subm x y = addm x (negm y)
 negm     = map (map negate)
 sqm      = map (map square)
 
-func1 :: [[Complex Double]] -> [[Complex Double]]
+func1 :: [[ℂ]] -> [[ℂ]]
 func1 x = ifft2d $ fft2d x `divm` (sqm wavenum `addm` sqm im `addm` sqm jm)
 
-eps :: Double
+eps :: ℝ
 eps = 0.01
 
-delta :: Double -> Double -> Double -> Double -> Complex Double
+delta :: ℝ -> ℝ -> ℝ -> ℝ -> ℂ
 delta cx cy x y = if norm ((x / size) - cx) ((y / size) - cy) < eps then 1 else 0
 
-matrix :: [[Complex Double]]
+matrix :: [[ℂ]]
 matrix = sample2D testFunc [0, 1 .. size - 1] [0, 1 .. size - 1]
 
-rendCmp :: Complex Double -> (Double, Double, Double)
+rendCmp :: ℂ -> (ℝ, ℝ, ℝ)
 rendCmp c = uncurryRGB (\x y z -> (x, y, z)) $ hsl a 0.25 m
   where
     m = magnitude c
     a = (* (180 / pi)) $ (+ pi) $ phase c
 
-test1 :: [[Complex Double]]
+test1 :: [[ℂ]]
 test1 = matrix
-test2 :: [[Complex Double]]
+test2 :: [[ℂ]]
 test2 = ifft2d $ fft2d matrix
-test3 :: [[Complex Double]]
+test3 :: [[ℂ]]
 test3 = func1 matrix
 
 lengthX :: [[a]] -> Int
@@ -103,26 +104,26 @@ index i j l
   | j >= lengthY l = error $ "y out of bounds: " ++ show j ++ " > " ++ show (lengthY l)
   | otherwise      = (l !! i) !! j
 
-myfun :: Double -> Double -> (Double, Double, Double)
+myfun :: ℝ -> ℝ -> (ℝ, ℝ, ℝ)
 myfun i j = rendCmp $ index (round $ i * (size - 1)) (round $ j * (size - 1)) test3
 
-rotate :: Complex Double -> Complex Double
+rotate :: ℂ -> ℂ
 rotate = uncurry mkPolar . rot . polar
   where
     rot (m, p) = (m, p + 0.1)
 
-visual :: (Double, Double, Double, Double) -> Complex Double -> Complex Double
+visual :: (ℝ, ℝ, ℝ, ℝ) -> ℂ -> ℂ
 visual (xa, xb, ya, yb) (polar -> (m, p))
   = mkPolar (((yb - ya) * (m - xa)/(xb - xa)) + ya) p
 
-visualM :: [[Complex Double]] -> [[Complex Double]]
+visualM :: [[ℂ]] -> [[ℂ]]
 visualM cs = map (map (visual (small, big, 0, 1))) cs
   where
     mags  = concatMap (map magnitude) cs
     small = minimum mags
     big   = maximum mags
 
-config :: Conf [[Complex Double]]
+config :: Conf [[ℂ]]
 config = Conf { keyBinds = const id
               , state = test3
               , evolve = map (map rotate)
--- library/SatOpt/Utility.hs
+++ library/SatOpt/Utility.hs
@@ -1,9 +1,46 @@
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 -- | TODO
 module SatOpt.Utility where
 
+import           Data.Packed.Matrix            (liftMatrix2)
+import           Data.Packed.Vector            (zipVectorWith)
+import           Numeric.LinearAlgebra.Data
+import           Numeric.LinearAlgebra.HMatrix
+
+zipMatrixWith ::(Element a, Element b, Element c)
+                => (a -> b -> c)
+                -> Matrix a
+                -> Matrix b
+                -> Matrix c
+zipMatrixWith f = liftMatrix2 (zipVectorWith f)
 
 sample2D :: (a -> a -> b) -> [a] -> [a] -> [[b]]
 sample2D f xs ys = [[f x y | y <- ys] | x <- xs]
+
+--class Conv a b | a -> b where
+class Conv a b where
+  convert :: a -> b
+
+instance Conv Double  Int where convert = round
+instance Conv Float   Int where convert = round
+instance Conv Int     Int where convert = id
+instance Conv Integer Int where convert = fromIntegral
+
+sample2DM :: (Container Vector a, Container Matrix b, Indexable (Vector a) a, Conv b Int)
+             => (a -> a -> b)
+             -> Vector a
+             -> Vector a
+             -> Matrix b
+sample2DM f vx vy = build (size vx, size vy) (\i j -> f (vx ! convert i) (vy ! convert j))
+--  where
+--    rows = fromRows $ replicate (size vy) vx
+--    columns = fromColumns $ replicate (length vx) vy
+-- fromColumns $ replicate (length testx) (fromList testy)
+-- fromRows $ replicate (length testy) (fromList testx)
+-- let zipMatrix f = liftMatrix2 (zipVectorWith f)
 
 square :: Num a => a -> a
 square x = x * x
