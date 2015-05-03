1c1abb7c654c147c274bd0ecaceb636079945757
Remy Goldschmidt <taktoa@gmail.com>
2015-05-02 20:59:35 -0500

    Switching to a more abstract representation of state

--- library/SatOpt.hs
+++ library/SatOpt.hs
@@ -14,8 +14,67 @@ import           Data.Colour.RGBSpace
 import           Data.Colour.RGBSpace.HSL
 import           Data.Complex
 import           Data.Packed.Matrix            (mapMatrix)
-import           Numeric.LinearAlgebra.HMatrix hiding (matrix, size)
+import           Numeric.LinearAlgebra.HMatrix hiding (disp, matrix, size)
 --import qualified Numeric.LinearAlgebra.HMatrix as HM
+
+data Field2D a = FMat  (Matrix a)
+               | FFT   (Matrix a)
+               | FDisp (Matrix a)
+               | FFunc (ℝ -> ℝ -> a)
+
+data State = State { amplitude  :: Field2D ℂ
+                   , dispersion :: Field2D ℂ
+                   , sources    :: Field2D ℂ
+                   , time       :: ℝ
+                   , freq       :: ℝ
+                   , concrete   :: Bool
+                   }
+
+constF :: Element a => a -> Field2D a
+constF x = matField (FFunc (\_ _ -> x))
+
+defState :: State
+defState = State { amplitude  = constF (0 :+ 0)
+                 , dispersion = constF (1 :+ 0)
+                 , sources    = constF (0 :+ 0)
+                 , time       = 0
+                 , freq       = 1
+                 , concrete   = False
+                 }
+
+matField :: Element a => Field2D a -> Field2D a
+matField (FFunc f) = FMat $ sample2DM f [0, 1 .. size - 1] [0, 1 .. size - 1]
+matField x         = x
+
+fftMat :: Field2D ℂ -> Matrix ℂ
+fftMat (FFT m)  = m
+fftMat (FMat m) = fft2dM m
+fftMat f        = fftMat $ matField f
+
+dispMat :: Field2D ℂ -> Matrix ℂ
+dispMat (FDisp m) = m
+dispMat (FMat m)  = recip $ square m + square im + square jm
+dispMat f         = dispMat $ matField f
+
+makeConcrete :: State -> State
+makeConcrete st@(State { concrete = True }) = st
+makeConcrete st@(State { dispersion = d, sources = s, time = t, freq = f })
+  = st { amplitude = ampl, dispersion = FDisp disp, sources = FFT srcs, concrete = True }
+  where
+    disp = dispMat $ matField d
+    srcs = fftMat $ matField s
+    ampl = FMat $ timeShift f t $ ifft2dM $ srcs * disp
+
+phaseShift :: Matrix ℝ -> Matrix ℂ -> Matrix ℂ
+phaseShift p m = mags * zipMatrixWith (:+) (cos angles) (sin angles)
+  where
+    angles = p + (mapMatrix realPart m `arctan2` mapMatrix imagPart m)
+    mags = m * conj m
+
+timeShift :: ℝ -> ℝ -> Matrix ℂ -> Matrix ℂ
+timeShift f t m = case matField $ constF (2 * pi * f * t) of
+                   FMat p -> phaseShift p m
+                   _      -> error "matField output invalid"
 
 size :: Num a => a
 size = 256
@@ -33,14 +92,17 @@ window x y = (+ 0.5) $ clamp $ subtract 0.5 $ (* (size/8)) $ recip $ cnorm x y
 pointSources :: [(ℝ, ℝ, ℂ)] -> ℝ -> ℝ -> ℂ
 pointSources ps x y = sum $ map (\(cx, cy, s) -> s * delta cx cy x y) ps
 
-
 testFunc :: ℝ -> ℝ -> ℂ
 testFunc = pointSources [ (0.48, 0.48, 0.5 :+ 0)
                         , (0.52, 0.52, (-0.5) :+ 0)
                         ]
 
+wavenumF :: ℝ -> ℝ -> ℂ
+wavenumF _ _ = 1 :+ 0
+
 wavenum :: Matrix ℂ
-wavenum = mapMatrix (const (1 :+ 0)) im
+wavenum = sample2DM wavenumF [0, 1 .. size - 1] [0, 1 .. size - 1]
+--  mapMatrix (const (1 :+ 0)) im
 
 im, jm :: Matrix ℂ
 im = sample2DM (\x _ -> x :+ 0) [0, 1 .. size - 1] [0, 1 .. size - 1]
